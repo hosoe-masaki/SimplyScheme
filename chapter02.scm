bc919a5d471e2a5394a781c5da351ae100c62f97
HOSOE Masaki <jose.no20@gmail.com>
2013-09-29 22:17:01 +0900

    Chapter 2 で入力したプログラムなど

---
+++ chapter02.scm
@@ -0,0 +1,24 @@
+; = Chapter 2 Functions
+
+(load "functions.scm")
+(functions)
+
+; == Arithmetic
+
+; == Words
+; The technical term for "a kind of data" is a type.
+
+; == Domain and Range
+; The technical term for "the things that a function accepts as an argument" is the domain of the function.
+; The name for "the things that a function returns" is its range.
+
+; == More Types: Sentences and Booleans
+; sentence: a bunch of words enclosed in parentheses
+; The two values "true" and "false" are called Booleans
+
+; == Our Favorite Type: Functions
+; A function can be used as data.
+
+; == Play with It
+
+; == Thinking about What You've Done
